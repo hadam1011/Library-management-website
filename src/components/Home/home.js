93bfa65265b0da66dbe3c3526242c9935aa93f03
Đàm <73334675+hadam1011@users.noreply.github.com>
2023-03-02 14:29:36 +0700

    update header

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -1,14 +1,17 @@
+import { Link } from "react-router-dom";
 import './home.css';
 
 function Homepage() {
     return (
         <div className='container'>
             <div className='header'>
-                <h3 id="header-left">Library Manager</h3>
-                <span>
-                    <h3 id="header-Homepage">Log out</h3>
-                    <h3 id="header-Homepage">Admin</h3>
-                </span>
+                <div className="left-items">
+                    <h3 id="header-Homepage">Library Manager</h3>
+                </div>
+                <div className="right-items">
+                    <Link to="/" id="header-Homepage">Log out</Link>
+                    <Link to="/userInfo" id="header-Homepage">Admin</Link>
+                </div>
             </div>
             <div className='sidebar'></div>
             <div className='content'></div>
